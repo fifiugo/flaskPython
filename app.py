d5c977a1c9b6ccded36351c7a5b5a7382728769b
fifiugo <duval.c82@gmail.com>
2021-09-01 17:28:10 +0200

    Fixed bug db

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+#@app.before_first_request
+#def create_tables():
+#    db.create_all()
 
 
 jwt = JWT(app, authenticate, identity) #crée un endpoint /auth
@@ -29,6 +29,5 @@ api.add_resource(Store, '/store/<name>')
 api.add_resource(StoreList, '/stores')
 
 if __name__ == '__main__':
-    from db  import db
     db.init_app(app)
     app.run(port=5000, debug = True)
