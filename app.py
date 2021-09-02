367245431ccef5fb12952925f6adda18cf2a4858
fifiugo <71088966+fifiugo@users.noreply.github.com>
2021-09-02 10:44:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db') "local dev
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgres://mpjoqnoizmpkkn:d1026953a18d2808539')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', 'sqlite:///data.db') 
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgres://mpjoqnoizmpkkn:d1026953a18d2808539')
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
